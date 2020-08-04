3dc06dacf6b82deab279b5238b998282ad78d475
Riddhi Dhakate <riddhi.dhakate@brainvire.com>
2020-08-04 18:37:27 +0530

    store-pickup-method added dropdown in checkout page

---
+++ StorePickupMethod/Block/Adminhtml/Order/View/Info.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Brainvire\StorePickupMethod\Block\Adminhtml\Order\View;
+
+use Magento\Eav\Model\AttributeDataFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Sales\Model\Order\Address;
+
+/**
+ * @api
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ * @since 100.0.2
+ */
+class Info extends \Magento\Sales\Block\Adminhtml\Order\View\Info
+{
+    /**
+     * Customer service
+     *
+     * @var \Magento\Customer\Api\CustomerMetadataInterface
+     */
+    protected $metadata;
+
+    /**
+     * Group service
+     *
+     * @var \Magento\Customer\Api\GroupRepositoryInterface
+     */
+    protected $groupRepository;
+
+    /**
+     * Metadata element factory
+     *
+     * @var \Magento\Customer\Model\Metadata\ElementFactory
+     */
+    protected $_metadataElementFactory;
+
+    /**
+     * @var Address\Renderer
+     */
+    protected $addressRenderer;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Sales\Helper\Admin $adminHelper
+     * @param \Magento\Customer\Api\GroupRepositoryInterface $groupRepository
+     * @param \Magento\Customer\Api\CustomerMetadataInterface $metadata
+     * @param \Magento\Customer\Model\Metadata\ElementFactory $elementFactory
+     * @param \Magento\Sales\Model\Order\Address\Renderer $addressRenderer
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Sales\Helper\Admin $adminHelper,
+        \Magento\Customer\Api\GroupRepositoryInterface $groupRepository,
+        \Magento\Customer\Api\CustomerMetadataInterface $metadata,
+        \Magento\Customer\Model\Metadata\ElementFactory $elementFactory,
+        \Magento\Sales\Model\Order\Address\Renderer $addressRenderer,
+        \Magento\Quote\Model\QuoteRepository $quoteRepository,
+        array $data = []
+    ) {
+        $this->groupRepository = $groupRepository;
+        $this->metadata = $metadata;
+        $this->_metadataElementFactory = $elementFactory;
+        $this->addressRenderer = $addressRenderer;
+        $this->quoteRepository = $quoteRepository;
+        parent::__construct($context, $registry, $adminHelper,$groupRepository, $metadata,$elementFactory,$addressRenderer,$data);
+    }
+
+    public function getQuote(){
+        return $this->quoteRepository;
+    }
+  
+}
---
+++ StorePickupMethod/view/frontend/web/js/form/element/select.js
@@ -0,0 +1,27 @@
+define([
+    'jquery',
+    'ko',
+    'uiComponent',
+    'Magento_Checkout/js/model/quote'
+], function ($, ko, Component, quote) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Brainvire_StorePickupMethod/checkout/shipping/storepickup'
+        },
+
+        initObservable: function () {
+
+            this.selectedMethod = ko.computed(function() {
+                var method = quote.shippingMethod();
+                var selectedMethod = method != null ? method.carrier_code + '_' + method.method_code : null;
+                return selectedMethod;
+            }, this);
+
+            return this;
+        },
+        selectedAddress : ko.observable(),
+        availableAddresses : ko.observableArray(window.checkoutConfig.storepick_config),
+    });
+});
